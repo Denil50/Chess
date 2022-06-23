8089c9f71b95c01197bbc7c8a2b5dd7ef09a1b13
Apple <apple@MacBook-Pro.local>
2022-06-23 12:46:33 +0300

    update v2

--- Chess/BoardView.swift
+++ Chess/BoardView.swift
@@ -9,18 +9,49 @@ import UIKit
 
 class BoardView: UIView {
 
-    let ratio: CGFloat = 0.8
-    var coordinateX: CGFloat = -1
-    var coordinateY: CGFloat = -1
-    var sizeCell: CGFloat = -1
+    let ratio = 0.95
+    var coordinateX = -10
+    var coordinateY = -10
+    var sizeCell = -10
     
+    var shadowPieces: Set<ChessPiece> = Set<ChessPiece>()
     
     override func draw(_ rect: CGRect) {
-        coordinateX = bounds.width * (1 - ratio) / 2
-        coordinateY = bounds.height * (1 - ratio) / 2
-        sizeCell = bounds.width * ratio / 8
-        drawBoard()
+        coordinateX = Int(bounds.width * (1 - ratio) / 2)
+        coordinateY = Int(bounds.height * (1 - ratio) / 2)
+        sizeCell = Int(bounds.width * ratio / 8)
         
+        drawBoard()
+        drawPieces()
+    }
+    
+    func drawPieces() {
+        for piece in shadowPieces {
+            let piecesImage = UIImage(named: piece.imageName)
+            piecesImage?.draw(in: (CGRect(x: coordinateX + (piece.col * sizeCell),
+                                          y: coordinateY + (piece.row * sizeCell),
+                                          width: sizeCell, height: sizeCell)))
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let first = touches.first!
+        let fingerLocation = first.location(in: self)
+        
+        print(fingerLocation)
+        
+        let fromCol = (Int(fingerLocation.x) - coordinateX) / sizeCell
+        let fromRow = (Int(fingerLocation.y) - coordinateX) / sizeCell
+        print("FROMx: \(fromCol), y: \(fromRow)")
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let first = touches.first!
+        let fingerLocation = first.location(in: self)
+        
+        let toCol = (Int(fingerLocation.x) - coordinateX) / sizeCell
+        let toRow = (Int(fingerLocation.y) - coordinateX) / sizeCell
+        print("TOx: \(toCol), y: \(toRow)")
     }
     
     func drawBoard() {
@@ -33,18 +64,14 @@ class BoardView: UIView {
                 drawSquare(col: (col * 2) + 1, row: (row * 2) + 1, color: UIColor.white)
             }
         }
-            
-        
     }
-    
+
     func drawSquare(col: Int, row: Int, color: UIColor) {
-        let path = UIBezierPath(rect: CGRect(x: coordinateX + (CGFloat(col) * sizeCell),
-                                             y: coordinateY + (CGFloat(row) * sizeCell),
+        let path = UIBezierPath(rect: CGRect(x: coordinateX + (col * sizeCell),
+                                             y: coordinateY + (row * sizeCell),
                                              width: sizeCell, height: sizeCell))
         color.setFill()
         path.fill()
-        
-        
     }
 
 }
